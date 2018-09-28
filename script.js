c36e5a540b866deab7280ea5a017e7f8255853c0
github username <github email address>
2018-09-28 14:37:41 -0400

    css, html, js, sounds

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', (event) => {
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; // remind to start
+  audio.play();
+  key.classList.add('playing');
+});
+
+const removeTransition = (event) => {
+  if (event.propertyName !== "transform") return;
+  console.log(event.propertyName);
+  this.classList.remove('playing')
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
